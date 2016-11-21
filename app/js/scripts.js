90dfe515d5089a2f896010a62e51850cab8a4b54
alexandraklein <ali.klein@deutschinc.com>
2016-11-21 12:03:09 -0800

    Img tags + styles

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,16 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-// do your setup here
+
     var $ = require('jquery');
 
     $(function() {
         console.log('scripts.js Loaded');
-        $(document).ready(function() {
-            $('#fullpage').fullpage({
-                navigation: true,
-                slidesNavigation: true,
-                lazyLoading: true,
-                verticalCentered: false
-            });
+
+        $('#fullpage').fullpage({
+            navigationPosition: 'left',
+            navigation: true,
+            loopHorizontal: false,
+            slidesNavigation: true,
+            lazyLoading: true,
+            verticalCentered: false,
+            css3: true,
+            easingcss3: 'ease-out',
+            dragAndMove: true
         });
     });
 });
